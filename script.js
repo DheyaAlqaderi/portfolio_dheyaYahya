099d579c70795c633816f478ed8d11ba6f26c530
DheyaAlqaderi <116699614+DheyaAlqaderi@users.noreply.github.com>
2024-09-20 00:54:50 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// JavaScript for toggling the nav
 document.getElementById('navToggle').addEventListener('click', function() {
     var navMenu = document.getElementById('navMenu');
     navMenu.classList.toggle('active');
